142eff3edcd125ffec2cf044538f5dd2aa8f6e22
ongyiren <OYRY5.16@ichat.sp.edu.sg>
2018-02-01 11:27:20 +0800

    Updated ECommerceFacadeREST.java

--- src/java/service/ECommerceFacadeREST.java
+++ src/java/service/ECommerceFacadeREST.java
@@ -1,17 +1,25 @@
 package service;
 
+import DBModelLayer.itementityDB;
+import DBModelLayer.lineitementityDB;
+import DBModelLayer.salesrecordentityDB;
+import DBModelLayer.salesrecordentity_lineitementityDB;
+import DBModelLayer.storeentityDB;
 import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.QueryParam;
@@ -43,5 +51,59 @@ public class ECommerceFacadeREST {
     @PUT
     @Consumes("application/json")
     public void putJson(String content) {
+    }
+    
+     @POST
+    @Path("CreateECommerceTransactionRecord")
+    @Consumes("application/x-www-form-urlencoded")
+    public Response CreateECommerceTransactionRecord(@FormParam("amountPaid") double amountPaid,
+            @FormParam("memberId") int memberId, @FormParam("storeName") String storeName) {
+            
+            try{
+                storeentityDB storeDb = new storeentityDB();
+                int storeId = storeDb.getStoreId(storeName);
+                
+                salesrecordentityDB salesRecordDb = new salesrecordentityDB();
+                int salesRecordId = salesRecordDb.insertSalesRecord(amountPaid, memberId, storeId);
+                
+                return Response.status(200).entity(salesRecordId).build();
+            }catch(Exception ex){
+                ex.printStackTrace();
+                return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+            }
+    }
+    
+    @POST
+    @Path("CreateECommerceLineItemRecord")
+    @Consumes("application/x-www-form-urlencoded")
+    public Response CreateECommerceLineItemRecord(@FormParam("quantity") int quantity, @FormParam("SKU") String sku, @FormParam("transactionRecordId") int transactionRecordId) {
+            
+            try{
+                itementityDB itemDb = new itementityDB();
+                int itemId = itemDb.getItemId(sku);
+                
+                lineitementityDB lineItemDb = new lineitementityDB();
+                int lineItemId = lineItemDb.insertLineItemRecord(quantity, itemId);
+                
+                salesrecordentity_lineitementityDB srliDb = new salesrecordentity_lineitementityDB();
+                srliDb.insertSalesRecord_LineItemRecord(transactionRecordId, lineItemId);
+                
+                int dbQuantity = lineItemDb.getECommerceStoreQuantity(itemId);
+                dbQuantity -= quantity;
+                
+                lineItemDb.updateECommerceStoreQuantity(dbQuantity, itemId);
+                
+                return Response
+                    .status(200)
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                    .header("Access-Control-Allow-Credentials", "true")
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                    .header("Access-Control-Max-Age", "1209600")
+                    .build();
+            }catch(Exception ex){
+                ex.printStackTrace();
+                return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+            }
     }
 }
